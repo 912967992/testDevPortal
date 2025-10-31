aed2613599ebeea5b3f83ccfadc61949a70c8c7c
912967992 <912967992@qq.com>
2025-10-31 11:00:45 +0800

    20251031新增系统主页

--- src/main/java/com/lu/ddwyydemo04/controller/reliabilityLab/Index.java
+++ src/main/java/com/lu/ddwyydemo04/controller/reliabilityLab/Index.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class Index {
     @GetMapping("/")
     public String root() {
-        return "reliablityLab/reliabilityIndex";
+        return "home";
     }
 
     @GetMapping("/reliablityLab/index") // 处理页面跳转请求
