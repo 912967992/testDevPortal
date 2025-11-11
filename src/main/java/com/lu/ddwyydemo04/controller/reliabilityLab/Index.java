5aded20e41d334d2988bb2e0a923c25a289b720c
912967992 <912967992@qq.com>
2025-11-11 11:06:03 +0800

    20251111删除掉项目多余的内容，接下来准备开始完善代码软硬件互联

--- src/main/java/com/lu/ddwyydemo04/controller/reliabilityLab/Index.java
+++ src/main/java/com/lu/ddwyydemo04/controller/reliabilityLab/Index.java
@@ -23,11 +23,11 @@ public class Index {
         return "home";
     }
 
-    @GetMapping("/reliablityLab/index") // 处理页面跳转请求
-    public String loginUsageRate() {
-        // 返回跳转页面的视图名称
-        return "reliablityLab/reliabilityIndex";
-    }
+//    @GetMapping("/reliablityLab/index") // 处理页面跳转请求
+//    public String loginUsageRate() {
+//        // 返回跳转页面的视图名称
+//        return "reliablityLab/reliabilityIndex";
+//    }
 
 
     // 跳转到物联网温箱监控页面模板（reliabilityIndex.html）
